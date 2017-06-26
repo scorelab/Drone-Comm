2098815cdab39b1980450eda6ea73a632bacb8f2
amila1991 <amilakarunathilaka.12@cse.mrt.ac.lk>
2017-06-26 23:24:01 +0530

    set base template component

---
+++ webapp/src/app/components/base/base.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+/**
+ * @author Amila Karunathilaka
+ */
+@Component({
+  selector: 'drone-comm-base',
+  templateUrl: './base.component.html',
+  styleUrls: ['./base.component.css']
+})
+export class BaseComponent {
+  title = 'Drone Communnity';
+}
