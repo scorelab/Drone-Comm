e57341dfa9741b84c54cc6592947971b7639b0df
amila1991 <amilakarunathilaka.12@cse.mrt.ac.lk>
2017-07-16 23:41:37 +0530

    add email verification backend services

---
+++ app/dao/emialVerificationTokenDAO.js
@@ -0,0 +1,29 @@
+/**
+ * @author Amila Karunathilaka
+ */
+
+const EmailVerificationToken = require('../model/emailVerificationToken');
+
+emailVerificationTokenDAO = {};
+
+emailVerificationTokenDAO.insert = function (token, callback) {
+    var tokenObj = new EmailVerificationToken(token);
+    tokenObj.save(function (err) {
+        callback(err);
+    })
+};
+
+
+emailVerificationTokenDAO.getToken = function (token, callback) {
+    EmailVerificationToken.findOne({token:token}, function (err, verificationToken) {
+        callback(err, verificationToken);
+    })
+};
+
+emailVerificationTokenDAO.getTokenByUserId = function (userId, callback) {
+    EmailVerificationToken.findOne({userId:userId}, function (err, verificationToken) {
+        callback(err, verificationToken);
+    })
+};
+
+module.exports = emailVerificationTokenDAO;
---
+++ app/service/emailVerificationTokenService.js
@@ -0,0 +1,69 @@
+/**
+ * @author Amila Karunathilaka
+ */
+
+const droneCommServiceError = require('../util/error/DroneCommServiceError');
+
+const uuidv4 = require('uuid/v4');
+
+const emailVerificationTokenDAO = require('../dao/emialVerificationTokenDAO');
+
+const userService = require('./userService');
+const profileService = require('./profileService');
+
+emailVerificationTokenService = {};
+
+emailVerificationTokenService.createToken = function (userId, callback) {
+    var token = uuidv4();
+    var date = Date.now();
+    date += (1000 * 60 * 60 * 24);
+    var verificationToken = {
+        userId: userId,
+        token: token,
+        expiredDate: new Date(date)
+    };
+    emailVerificationTokenDAO.insert(verificationToken, function (err) {
+        if (err) {
+            return callback(new droneCommServiceError("Database Connection Error", err));
+        }
+        callback(null, token);
+    });
+};
+
+emailVerificationTokenService.verifyToken = function (token, callback) {
+    emailVerificationTokenDAO.getToken(verificationToken, function (err, verificationToken) {
+        if (err) {
+            return callback(new droneCommServiceError("Database Connection Error", err));
+        }
+        userService.activeUser(verificationToken.userId, function (err, success) {
+            if (err) {
+                return callback(err);
+            }
+            callback(null);
+        });
+    });
+};
+
+emailVerificationTokenService.resendToken = function (username, callback) {
+    userService.getUser(username, function (err, user) {
+        if (err) {
+            return callback(new droneCommServiceError("Database Connection Error", err));
+        }
+        emailVerificationTokenDAO.getToken(user._id, function (err, token) {
+            if (err) {
+                return callback(new droneCommServiceError("Database Connection Error", err));
+            }
+            profileService.getProfileByUsername(username, function (err, profile) {
+               if (err) {
+                   return callback(new droneCommServiceError("Database Connection Error", err));
+               }
+                callback(null, {
+                    token: token,
+                    email: resources.profile.email
+                });
+            });
+        });
+    });
+};
+
+module.exports = emailVerificationTokenService;
