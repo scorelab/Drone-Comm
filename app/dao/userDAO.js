50c9756af18594f0af2a1847d497b8f3ed36c841
amila1991 <amilakarunathilaka.12@cse.mrt.ac.lk>
2017-07-21 23:25:34 +0530

    update user data access functions

--- app/dao/userDAO.js
+++ app/dao/userDAO.js
@@ -19,10 +19,23 @@ userDAO.remove = function (username, callback) {
     })
 };
 
+userDAO.update = function (user, callback) {
+    var userObj = new User(user);
+    userObj.save(function (err) {
+        callback(err);
+    })
+}
+
 userDAO.getUser = function(name, callback) {
     User.findOne({name: name}, function (err, user) {
         callback(err, user);
     });
+};
+
+userDAO.getUserById = function (id, callback) {
+  User.findOne({_id: id}, function (err, user) {
+      callback(err, user);
+  });
 };
 
 module.exports = userDAO;
