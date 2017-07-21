72a26767ab4c32b531b9a14ea01eccd7fcd4f4fa
amila1991 <amilakarunathilaka.12@cse.mrt.ac.lk>
2017-07-21 23:32:19 +0530

    user registration back end with email verification and verification resend

--- app/route/authenticateRoute.js
+++ app/route/authenticateRoute.js
@@ -3,7 +3,11 @@
  */
 
 var express = require('express');
+
 var authService = require('../service/authenticationService');
+var verificationTokenService = require('../service/emailVerificationTokenService');
+
+var verificationMailSender = require('../util/verificationMailSender');
 
 var router = express.Router();
 
@@ -23,8 +27,53 @@ router.post('/register', function (req, res, next) {
         if (err) {
             return next(err);
         }
-        res.send(response);
+        verificationMailSender.sendMail(req.get('origin'), response.token, response.email, function (err, success) {
+            if (err) {
+                return next(err);
+            }
+
+            var resp = {
+                msg : 'Successfully registered user',
+                errorCode:  0
+            };
+            res.send(resp);
+        });
     });
+});
+
+
+router.get('/resend/:name', function (req, res, next) {
+    verificationTokenService.resendToken(req.params.name, function (err, response) {
+        if (err) {
+            return next(err);
+        }
+        verificationMailSender.sendMail(req.get('host'), response.token, response.email, function (err, success) {
+            if (err) {
+                return next(err);
+            }
+
+            var resp = {
+                msg : 'Successfully registered user',
+                errorCode:  0
+            };
+            res.send(resp);
+        });
+
+})
+});
+
+router.get('/verify', function (req, res, next) {
+    verificationTokenService.verifyToken(req.query.id, function (err) {
+        if (err) {
+            return next(err);
+        }
+
+        var response = {
+            msg : 'Successfully verified user',
+            errorCode:  0
+        };
+        res.send(response);
+    })
 });
 
 module.exports = router;
